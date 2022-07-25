3ad5baa1ac91758dd201ad4ea636d95f3c5dc441
XSS6 <72362725+XSS6-2b2t@users.noreply.github.com>
2022-07-25 11:38:58 -0600

    Update ui.py

--- main/src/UI/ui.py
+++ main/src/UI/ui.py
@@ -3,9 +3,9 @@ from time import sleep
 import os
 
 import sys
-
+import colorama
 import socket
-
+SysMon = True
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
@@ -14,3 +14,11 @@ s.close()
 OGG = 0.6
 
 print(f" OGG registrar: {OGG}")
+
+from colorama import init, Fore, Back, Style init(convert=SysMon)
+
+print(Fore.RED + 'FATAL ERROR')
+print(Back.GREEN + 'retrying')
+print(Style.DIM + 'connecting...')
+print(Style.RESET_ALL)
+print('SOCKET OPENED.)
