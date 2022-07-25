d5712e7493da156fefadda40c3754c00e95b0f3a
XSS6 <72362725+XSS6-2b2t@users.noreply.github.com>
2022-07-25 11:46:04 -0600

    Create setup.py

---
+++ main/src/setup.py
@@ -0,0 +1,9 @@
+name='PyShort',
+version='0.23',
+description='a python SSH tool',
+license='MIT',
+packages=['hellostackoverflow'],
+author='Benjamin Gerfelder',
+author_email='null',
+keywords=['SSH', 'TERMINAL', 'SOCKET'],
+url='https://github.com/XSS6_2b2t'
